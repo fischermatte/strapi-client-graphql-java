9b67c4a4962b01406e54068df55d4b1a99dba991
fischermatte <fischermatte@posteo.net>
2022-11-04 22:35:13 +0100

    initial commit

---
+++ src/test/java/dev/fischermatte/strapi/client/graphql/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fischermatte.strapi.client.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
