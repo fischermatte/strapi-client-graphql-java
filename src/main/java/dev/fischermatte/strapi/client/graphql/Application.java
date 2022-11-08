35db7017d44f87c4644e37c2e197219e57319cef
fischermatte <fischermatte@posteo.net>
2022-11-08 12:43:31 +0100

    chore: added first example

--- src/main/java/dev/fischermatte/strapi/client/graphql/Application.java
+++ src/main/java/dev/fischermatte/strapi/client/graphql/Application.java
@@ -1,13 +1,22 @@
 package dev.fischermatte.strapi.client.graphql;
 
+import com.graphql_java_generator.exception.GraphQLRequestExecutionException;
+import com.graphql_java_generator.exception.GraphQLRequestPreparationException;
+import dev.fischermatte.strapi.client.graphql.api.util.QueryExecutor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+
+    public static void main(String[] args) throws GraphQLRequestPreparationException, GraphQLRequestExecutionException {
+        var ctx = SpringApplication.run(Application.class, args);
+        var queryExecutor = ctx.getBean(QueryExecutor.class);
+        var objectResponse = queryExecutor.getEmailTemplatesResponseBuilder().withQueryResponseDef("").build();
+        var response = queryExecutor.emailTemplatesWithBindValues(objectResponse, null, null,null,null, null, null);
+
     }
+
 
 }
